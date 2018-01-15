c0f086711a8e20d96c50d69e217da619bf544c58
aserbao <1142803753@qq.com>
2018-01-16 00:35:14 +0800

    修改学习计划，先学习OpenGL，今天先休息，修好洗衣机，睡觉，休息

---
+++ app/src/main/java/com/aserbao/aserbaosandroid/AserbaoApplication.java
@@ -0,0 +1,17 @@
+package com.aserbao.aserbaosandroid;
+
+import android.app.Application;
+
+/**
+ * Created by aserbao on 2018 2018/1/15.23:27
+ * Email:1142803753@qq.com
+ * weixin: aserbao
+ */
+
+public class AserbaoApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
--- app/src/main/java/com/aserbao/aserbaosandroid/HomeActivity.java
+++ app/src/main/java/com/aserbao/aserbaosandroid/HomeActivity.java
@@ -1,13 +1,25 @@
 package com.aserbao.aserbaosandroid;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class HomeActivity extends AppCompatActivity {
+
+    @BindView(R.id.home_recycler_view)
+    RecyclerView mHomeRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ButterKnife.bind(this);
+        initView();
+    }
+
+    private void initView() {
     }
 }
