a0920b2e1a4195c29a62e674c21c3781e309035a
aserbao <1142803753@qq.com>
2018-01-15 23:26:21 +0800

    准备学习实现RecyclerView的使用

---
+++ app/src/main/java/com/aserbao/aserbaosandroid/opengl/OpenGlActivity.java
@@ -0,0 +1,15 @@
+package com.aserbao.aserbaosandroid.opengl;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.aserbao.aserbaosandroid.R;
+
+public class OpenGlActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_open_gl);
+    }
+}
