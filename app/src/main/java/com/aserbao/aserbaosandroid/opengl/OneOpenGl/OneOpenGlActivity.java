50ebb6c50b208b58329aa119ac3a4847198212f1
aserbao <1142803753@qq.com>
2018-01-16 23:11:25 +0800

    开始学习OpenGl，从基本概念入手,整理成体系

---
+++ app/src/main/java/com/aserbao/aserbaosandroid/opengl/OneOpenGl/OneOpenGlActivity.java
@@ -0,0 +1,15 @@
+package com.aserbao.aserbaosandroid.opengl.OneOpenGl;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.aserbao.aserbaosandroid.R;
+
+public class OneOpenGlActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_one_open_gl);
+    }
+}
--- app/src/main/java/com/aserbao/aserbaosandroid/opengl/OpenGlActivity.java
+++ app/src/main/java/com/aserbao/aserbaosandroid/opengl/OpenGlActivity.java
@@ -1,15 +1,34 @@
 package com.aserbao.aserbaosandroid.opengl;
 
-import android.support.v7.app.AppCompatActivity;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-import com.aserbao.aserbaosandroid.R;
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
 
 public class OpenGlActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_open_gl);
+        GLSurfaceView glSurfaceView = new GLSurfaceView(this);
+        glSurfaceView.setRenderer(new GLSurfaceView.Renderer() {
+            @Override
+            public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+
+            }
+
+            @Override
+            public void onSurfaceChanged(GL10 gl, int width, int height) {
+
+            }
+
+            @Override
+            public void onDrawFrame(GL10 gl) {
+
+            }
+        });
+        setContentView(glSurfaceView);
     }
 }
