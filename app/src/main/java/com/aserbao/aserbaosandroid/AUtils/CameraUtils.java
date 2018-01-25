4c3662f1363d0952ac51c636ff5ed43fa65f0e37
aserbao <1142803753@qq.com>
2018-01-25 11:44:33 +0800

    提交代码，一步步来

---
+++ app/src/main/java/com/aserbao/aserbaosandroid/AUtils/CameraUtils.java
@@ -0,0 +1,139 @@
+package com.aserbao.aserbaosandroid.AUtils;
+
+import android.app.Activity;
+import android.hardware.Camera;
+import android.util.Log;
+import android.view.Surface;
+
+import java.util.List;
+
+/**
+ * description:
+ * Created by aserbao on 2018/1/25.
+ */
+
+
+public class CameraUtils {
+    private static final String TAG = "CameraUtils";
+
+    /**
+     * Attempts to find a preview size that matches the provided width and height (which
+     * specify the dimensions of the encoded video).  If it fails to find a match it just
+     * uses the default preview size for video.
+     * <p>
+     * TODO: should do a best-fit match, e.g.
+     * https://github.com/commonsguy/cwac-camera/blob/master/camera/src/com/commonsware/cwac/camera/CameraUtils.java
+     */
+    public static void choosePreviewSize(Camera.Parameters parms, int width, int height) {
+        // We should make sure that the requested MPEG size is less than the preferred
+        // size, and has the same aspect ratio.
+        Camera.Size ppsfv = parms.getPreferredPreviewSizeForVideo();
+        if (ppsfv != null) {
+            Log.d(TAG, "Camera preferred preview size for video is " +
+                    ppsfv.width + "x" + ppsfv.height);
+        }
+
+        //for (Camera.Size size : parms.getSupportedPreviewSizes()) {
+        //    Log.d(TAG, "supported: " + size.width + "x" + size.height);
+        //}
+
+        for (Camera.Size size : parms.getSupportedPreviewSizes()) {
+            if (size.width == width && size.height == height) {
+                parms.setPreviewSize(width, height);
+                return;
+            }
+        }
+
+        Log.w(TAG, "Unable to set preview size to " + width + "x" + height);
+        if (ppsfv != null) {
+            parms.setPreviewSize(ppsfv.width, ppsfv.height);
+        }
+        // else use whatever the default size is
+    }
+
+    /**
+     * Attempts to find a fixed preview frame rate that matches the desired frame rate.
+     * <p>
+     * It doesn't seem like there's a great deal of flexibility here.
+     * <p>
+     * TODO: follow the recipe from http://stackoverflow.com/questions/22639336/#22645327
+     *
+     * @return The expected frame rate, in thousands of frames per second.
+     */
+    public static int chooseFixedPreviewFps(Camera.Parameters parms, int desiredThousandFps) {
+        List<int[]> supported = parms.getSupportedPreviewFpsRange();
+
+        for (int[] entry : supported) {
+            //Log.d(TAG, "entry: " + entry[0] + " - " + entry[1]);
+            if ((entry[0] == entry[1]) && (entry[0] == desiredThousandFps)) {
+                parms.setPreviewFpsRange(entry[0], entry[1]);
+                return entry[0];
+            }
+        }
+
+        int[] tmp = new int[2];
+        parms.getPreviewFpsRange(tmp);
+        int guess;
+        if (tmp[0] == tmp[1]) {
+            guess = tmp[0];
+        } else {
+            guess = tmp[1] / 2;     // shrug
+        }
+
+        Log.d(TAG, "Couldn't find match for " + desiredThousandFps + ", using " + guess);
+        return guess;
+    }
+
+    /**
+     * reference http://www.jianshu.com/p/1513134733d0
+     */
+    public static void setCameraDisplayOrientation(Activity activity,
+                                                   int cameraId, Camera camera) {
+        Camera.CameraInfo info = new android.hardware.Camera.CameraInfo();
+        Camera.getCameraInfo(cameraId, info);
+        int rotation = activity.getWindowManager().getDefaultDisplay().getRotation();
+        int degrees = 0;
+        switch (rotation) {
+            case Surface.ROTATION_0:
+                degrees = 0;
+                break;
+            case Surface.ROTATION_90:
+                degrees = 90;
+                break;
+            case Surface.ROTATION_180:
+                degrees = 180;
+                break;
+            case Surface.ROTATION_270:
+                degrees = 270;
+                break;
+        }
+
+        int result;
+        if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+            result = (info.orientation + degrees) % 360;
+            result = (360 - result) % 360;  // compensate the mirror
+        } else {  // back-facing
+            result = (info.orientation - degrees + 360) % 360;
+        }
+        camera.setDisplayOrientation(result);
+    }
+
+    //TODO: follow the recipe from http://stackoverflow.com/questions/22639336/#22645327
+    public static int[] closetFramerate(Camera.Parameters parameters, float frameRate) {
+        int framerate = (int) (frameRate * 1000);
+        List<int[]> rates = parameters.getSupportedPreviewFpsRange();
+        int[] bestFramerate = rates.get(0);
+        for (int i = 0; i < rates.size(); i++) {
+            int[] rate = rates.get(i);
+            Log.e(TAG, "supported preview pfs min " + rate[0] + " max " + rate[1]);
+            int curDelta = Math.abs(rate[1] - framerate);
+            int bestDelta = Math.abs(bestFramerate[1] - framerate);
+            if (curDelta < bestDelta) {
+                bestFramerate = rate;
+            } else if (curDelta == bestDelta) {
+                bestFramerate = bestFramerate[0] < rate[0] ? rate : bestFramerate;
+            }
+        }
+        return bestFramerate;
+    }
+}
---
+++ app/src/main/java/com/aserbao/aserbaosandroid/opengl/openGlCamera/FuCameraActivity.java
@@ -0,0 +1,201 @@
+package com.aserbao.aserbaosandroid.opengl.openGlCamera;
+
+import android.app.Activity;
+import android.graphics.SurfaceTexture;
+import android.hardware.Camera;
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Surface;
+import android.widget.Toast;
+
+import com.aserbao.aserbaosandroid.AUtils.CameraUtils;
+import com.aserbao.aserbaosandroid.R;
+
+import java.io.IOException;
+import java.lang.ref.WeakReference;
+import java.util.List;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class FuCameraActivity extends AppCompatActivity implements SurfaceTexture.OnFrameAvailableListener, Camera.PreviewCallback {
+    private static final String TAG = "FuCameraActivity";
+    int currentCameraType = Camera.CameraInfo.CAMERA_FACING_FRONT,cameraWidth = 1280,cameraHeight = 720;
+    final int PREVIEW_BUFFER_COUNT = 3;
+    byte[] mCameraNV21Byte;
+    byte[][] previewCallbackBuffer;
+    boolean isInPause = false,isNeedSwitchCameraSurfaceTexture = true;
+    Camera mCamera;
+    Handler mMainHandler;
+    @BindView(R.id.gl_surface_view)
+    GLSurfaceView mGlSurfaceView;
+    public GLRenderer mGLRenderer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fu_camera);
+        ButterKnife.bind(this);
+        mMainHandler = new MainHandler(this);
+
+        mGlSurfaceView.setEGLContextClientVersion(2);
+        mGLRenderer = new GLRenderer();
+        mGlSurfaceView.setRenderer(mGLRenderer);
+        mGlSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        openCamera(currentCameraType, cameraWidth, cameraHeight);
+        mGlSurfaceView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    private void openCamera(int cameraType, int desiredWidth, int desiredHeight) {
+        Camera.CameraInfo info = new Camera.CameraInfo();
+        int cameraId = 0;
+        int numCameras = Camera.getNumberOfCameras();
+        for (int i = 0; i < numCameras; i++) {
+            Camera.getCameraInfo(i, info);
+            if (info.facing == cameraType) {
+                cameraId = i;
+                mCamera = Camera.open(i);
+                currentCameraType = cameraType;
+                break;
+            }
+        }
+        if(mCamera == null){
+            Toast.makeText(this, "相机打开失败", Toast.LENGTH_SHORT).show();
+            onBackPressed();
+        }
+        CameraUtils.setCameraDisplayOrientation(this, cameraId, mCamera);
+        Camera.Parameters parameters = mCamera.getParameters();
+
+        /**
+         * 设置对焦，会影响camera吞吐速率
+         */
+        List<String> focusModes = parameters.getSupportedFocusModes();
+        if (focusModes.contains(Camera.Parameters.FOCUS_MODE_CONTINUOUS_VIDEO))
+            parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_VIDEO);
+
+        /**
+         * 设置fps
+         * */
+        int[] closetFramerate = CameraUtils.closetFramerate(parameters, 30);
+        Log.e(TAG, "closet framerate min " + closetFramerate[0] + " max " + closetFramerate[1]);
+        parameters.setPreviewFpsRange(closetFramerate[0], closetFramerate[1]);
+
+        CameraUtils.choosePreviewSize(parameters, desiredWidth, desiredHeight);
+        mCamera.setParameters(parameters);
+    }
+    
+    @Override
+    public void onFrameAvailable(SurfaceTexture surfaceTexture) {
+
+    }
+
+    @Override
+    public void onPreviewFrame(byte[] data, Camera camera) {
+        mCameraNV21Byte = isInPause ? null : data;
+        mCamera.addCallbackBuffer(data);
+    }
+
+    class GLRenderer implements GLSurfaceView.Renderer{
+        int mCameraTextureId = 1;
+        SurfaceTexture mCameraSurfaceTexture;
+        @Override
+        public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+
+        }
+
+        @Override
+        public void onSurfaceChanged(GL10 gl, int width, int height) {
+            GLES20.glViewport(0, 0, width, height);
+        }
+
+        @Override
+        public void onDrawFrame(GL10 gl) {
+            if (isNeedSwitchCameraSurfaceTexture) {
+                switchCameraSurfaceTexture();
+            }
+            float[] mtx = new float[16];
+            if (mCameraSurfaceTexture != null) {
+                try {
+                    mCameraSurfaceTexture.updateTexImage();
+                    mCameraSurfaceTexture.getTransformMatrix(mtx);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else throw new RuntimeException("HOW COULD IT HAPPEN!!! mCameraSurfaceTexture is null!!!");
+
+            if (mCameraNV21Byte == null || mCameraNV21Byte.length == 0) {
+                Log.e(TAG, "camera nv21 bytes null");
+                mGlSurfaceView.requestRender();
+                mGlSurfaceView.requestRender();
+                return;
+            }
+
+            if (!isInPause) mGlSurfaceView.requestRender();
+        }
+        public void switchCameraSurfaceTexture(){
+            isNeedSwitchCameraSurfaceTexture = false;
+            mCameraSurfaceTexture = new SurfaceTexture(mCameraTextureId);
+            mMainHandler.sendMessage(mMainHandler.obtainMessage(
+                    MainHandler.HANDLE_CAMERA_START_PREVIEW,
+                    mCameraSurfaceTexture));
+        }
+    }
+
+
+    static class MainHandler extends Handler {
+
+        static final int HANDLE_CAMERA_START_PREVIEW = 1;
+
+        private WeakReference<FuCameraActivity> mActivityWeakReference;
+
+        MainHandler(FuCameraActivity activity) {
+            mActivityWeakReference = new WeakReference<>(activity);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            FuCameraActivity activity = mActivityWeakReference.get();
+            switch (msg.what) {
+                case HANDLE_CAMERA_START_PREVIEW:
+                    activity.handleCameraStartPreview((SurfaceTexture) msg.obj);
+                    break;
+            }
+        }
+    }
+    private void handleCameraStartPreview(SurfaceTexture surfaceTexture) {
+        if (previewCallbackBuffer == null) {
+            Log.e(TAG, "allocate preview callback buffer");
+            previewCallbackBuffer = new byte[PREVIEW_BUFFER_COUNT][cameraWidth * cameraHeight * 3 / 2];
+        }
+        mCamera.setPreviewCallbackWithBuffer(this);
+        for (int i = 0; i < PREVIEW_BUFFER_COUNT; i++)
+            mCamera.addCallbackBuffer(previewCallbackBuffer[i]);
+        try {
+            mCamera.setPreviewTexture(surfaceTexture);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        surfaceTexture.setOnFrameAvailableListener(this);
+        mCamera.startPreview();
+    }
+
+}
