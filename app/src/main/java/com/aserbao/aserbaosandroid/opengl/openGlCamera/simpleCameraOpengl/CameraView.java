0ee9b6d7c342c05a79b435a0ea71c2b1a17c3f7f
aserbao <1142803753@qq.com>
2018-01-26 16:36:02 +0800

    将摄像头数据通过OpenGl进行处理成黑白颜色

--- app/src/main/java/com/aserbao/aserbaosandroid/opengl/openGlCamera/simpleCameraOpengl/CameraView.java
+++ app/src/main/java/com/aserbao/aserbaosandroid/opengl/openGlCamera/simpleCameraOpengl/CameraView.java
@@ -114,7 +114,7 @@ public class CameraView extends GLSurfaceView implements SurfaceTexture.OnFrameA
             int[] texture = new int[1];
 
             GLES20.glGenTextures(1, texture, 0);//生成一个OpenGl纹理
-            GLES20.glBindTexture(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, texture[0]);
+            GLES20.glBindTexture(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, texture[0]);//申请纹理存储区域并设置相关参数
             GLES20.glTexParameterf(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, GL10.GL_TEXTURE_MIN_FILTER,GL10.GL_LINEAR);
             GLES20.glTexParameterf(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, GL10.GL_TEXTURE_MAG_FILTER, GL10.GL_LINEAR);
             GLES20.glTexParameteri(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, GL10.GL_TEXTURE_WRAP_S, GL10.GL_CLAMP_TO_EDGE);
