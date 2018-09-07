a4fadf63d3af026dc6cd889ace634c30b21c6109
aserbao <aserbao@163.com>
2018-09-07 18:00:14 +0800

    提交代码

---
+++ app/src/main/java/com/aserbao/aserbaosandroid/functions/database/DataBaseActivity.java
@@ -0,0 +1,15 @@
+package com.aserbao.aserbaosandroid.functions.database;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.aserbao.aserbaosandroid.base.BaseActivity;
+
+public class DataBaseActivity extends BaseActivity {
+
+
+    @Override
+    public void initGetData() {
+
+    }
+}
