1019a7081e9cf2458c77caf2ae5d9768174f157c
aserbao <aserbao@163.com>
2018-09-26 19:20:21 +0800

    准备集成GreenDao

---
+++ app/src/main/java/com/aserbao/aserbaosandroid/database/DataBaseActivity.java
@@ -0,0 +1,14 @@
+package com.aserbao.aserbaosandroid.database;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.aserbao.aserbaosandroid.base.BaseActivity;
+
+public class DataBaseActivity extends BaseActivity {
+
+    @Override
+    public void initGetData() {
+
+    }
+}
---
+++ app/src/main/java/com/aserbao/aserbaosandroid/database/greenDao/GreenDaoActivity.java
@@ -0,0 +1,17 @@
+package com.aserbao.aserbaosandroid.database.greenDao;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.aserbao.aserbaosandroid.R;
+
+import butterknife.ButterKnife;
+
+public class GreenDaoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_green_dao);
+    }
+}
