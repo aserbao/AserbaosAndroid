8b5f3481e6638c7daf953969c6e7daab34819c71
aserbao <1142803753@qq.com>
2018-11-01 20:24:32 +0800

    提交代码，通过ndk-build将多个c源码编写成一个so库文件！

---
+++ app/obj/local/x86/objs/use_ndk_build_all/use_ndk_build2.o.d
@@ -0,0 +1,2 @@
+/Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/app/obj/local/x86/objs/use_ndk_build_all/use_ndk_build2.o: \
+  /Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/app/jni/use_ndk_build2.c
