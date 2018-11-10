73850c070d307229b0db4ca6cb052b09d30a5d9c
aserbao <aserbao@163.com>
2018-11-10 15:26:25 +0800

    提交代码

---
+++ aserbaoffmpeg/src/main/obj/local/arm64-v8a/objs/ffmpegdemo/aserbaoffmpeg.o.d
@@ -0,0 +1,83 @@
+/Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/obj/local/arm64-v8a/objs/ffmpegdemo/aserbaoffmpeg.o: \
+  /Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/aserbaoffmpeg.c \
+  /Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/com_example_aserbaoffmpeg_FFmpegUtils.h \
+  /Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/include/libavformat/avformat.h \
+  /Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/include/libavcodec/avcodec.h \
+  /Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/include/libavutil/samplefmt.h \
+  /Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/include/libavutil/avutil.h \
+  /Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/include/libavutil/common.h \
+  /Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/include/libavutil/attributes.h \
+  /Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/include/libavutil/macros.h \
+  /Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/include/libavutil/version.h \
+  /Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/include/libavutil/avconfig.h \
+  /Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/include/libavutil/mem.h \
+  /Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/include/libavutil/error.h \
+  /Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/include/libavutil/rational.h \
+  /Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/include/libavutil/mathematics.h \
+  /Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/include/libavutil/intfloat.h \
+  /Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/include/libavutil/log.h \
+  /Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/include/libavutil/pixfmt.h \
+  /Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/include/libavutil/buffer.h \
+  /Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/include/libavutil/cpu.h \
+  /Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/include/libavutil/channel_layout.h \
+  /Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/include/libavutil/dict.h \
+  /Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/include/libavutil/frame.h \
+  /Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/include/libavcodec/version.h \
+  /Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/include/libavformat/avio.h \
+  /Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/include/libavformat/version.h \
+  /Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/include/libavfilter/avfilter.h \
+  /Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/include/libavfilter/version.h
+
+/Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/com_example_aserbaoffmpeg_FFmpegUtils.h:
+
+/Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/include/libavformat/avformat.h:
+
+/Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/include/libavcodec/avcodec.h:
+
+/Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/include/libavutil/samplefmt.h:
+
+/Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/include/libavutil/avutil.h:
+
+/Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/include/libavutil/common.h:
+
+/Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/include/libavutil/attributes.h:
+
+/Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/include/libavutil/macros.h:
+
+/Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/include/libavutil/version.h:
+
+/Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/include/libavutil/avconfig.h:
+
+/Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/include/libavutil/mem.h:
+
+/Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/include/libavutil/error.h:
+
+/Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/include/libavutil/rational.h:
+
+/Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/include/libavutil/mathematics.h:
+
+/Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/include/libavutil/intfloat.h:
+
+/Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/include/libavutil/log.h:
+
+/Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/include/libavutil/pixfmt.h:
+
+/Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/include/libavutil/buffer.h:
+
+/Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/include/libavutil/cpu.h:
+
+/Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/include/libavutil/channel_layout.h:
+
+/Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/include/libavutil/dict.h:
+
+/Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/include/libavutil/frame.h:
+
+/Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/include/libavcodec/version.h:
+
+/Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/include/libavformat/avio.h:
+
+/Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/include/libavformat/version.h:
+
+/Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/include/libavfilter/avfilter.h:
+
+/Users/aserbao/aserbao/code/code/A_UI/AserbaosAndroid/AserbaosAndroid/aserbaoffmpeg/src/main/jni/include/libavfilter/version.h:
