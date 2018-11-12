ae37bc73aeb0d57dbb5537db3700fa9781a0e1c6
aserbao <aserbao@163.com>
2018-11-12 14:24:15 +0800

    提交分支SecondBranch

---
+++ aserbaoffmpeg/src/main/java/com/example/aserbaoffmpeg/Master.java
@@ -0,0 +1,11 @@
+package com.example.aserbaoffmpeg;
+
+/**
+ * 功能:
+ * author aserbao
+ * date : On 2018/11/12
+ * email: 1142803753@qq.com
+ */
+public class Master {
+    private String s = " 这是主分支的代码的第一次提交";
+}
---
+++ aserbaoffmpeg/src/main/java/com/example/aserbaoffmpeg/SecondBranch.java
@@ -0,0 +1,11 @@
+package com.example.aserbaoffmpeg;
+
+/**
+ * 功能:
+ * author aserbao
+ * date : On 2018/11/12
+ * email: 1142803753@qq.com
+ */
+public class SecondBranch {
+    private String s = " 这是second Branch 的第一次提交";
+}
