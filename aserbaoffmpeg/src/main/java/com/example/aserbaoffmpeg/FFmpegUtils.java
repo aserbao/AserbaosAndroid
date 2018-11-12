e09e1bfa4875f7b47129e21e559d9ad765ba9092
aserbao <aserbao@163.com>
2018-11-12 14:20:43 +0800

    提交代码,测试git

--- aserbaoffmpeg/src/main/java/com/example/aserbaoffmpeg/FFmpegUtils.java
+++ aserbaoffmpeg/src/main/java/com/example/aserbaoffmpeg/FFmpegUtils.java
@@ -7,6 +7,7 @@ package com.example.aserbaoffmpeg;
  * email: 1142803753@qq.com
  */
 public class FFmpegUtils {
+    private String s = " 这是Master";
     static {
         System.loadLibrary("avcodec");
         System.loadLibrary("avfilter");
