d8226e432e5d009ab99ef02be4c2c2c35d0f939e
aserbao <1142803753@qq.com>
2019-02-25 17:03:26 +0800

    提交代码

---
+++ app/src/main/java/com/aserbao/aserbaosandroid/aaSource/android/app/Activity/test/test.java
@@ -0,0 +1,13 @@
+package com.aserbao.aserbaosandroid.aaSource.android.app.Activity.test;
+
+/**
+ * 功能:
+ *
+ * @author aserbao
+ * @date : On 2019/2/12 2:59 PM
+ * @email: 1142803753@qq.com
+ * @project:AserbaosAndroid
+ * @package:com.aserbao.aserbaosandroid.aaSource.android.app.Activity.test
+ */
+public class test {
+}
---
+++ app/src/main/java/com/aserbao/aserbaosandroid/aaSource/android/material/MaterialActivity.java
@@ -0,0 +1,14 @@
+package com.aserbao.aserbaosandroid.aaSource.android.material;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.aserbao.aserbaosandroid.base.BaseActivity;
+
+public class MaterialActivity extends BaseActivity {
+
+    @Override
+    public void initGetData() {
+
+    }
+}
