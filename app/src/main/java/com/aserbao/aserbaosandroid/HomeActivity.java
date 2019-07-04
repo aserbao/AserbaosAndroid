414837f66ee3a7e19219898ec1d8b908333495ac
aserbao <1142803753@qq.com>
2019-07-04 22:37:18 +0800

    提交代码，okhttp

--- app/src/main/java/com/aserbao/aserbaosandroid/HomeActivity.java
+++ app/src/main/java/com/aserbao/aserbaosandroid/HomeActivity.java
@@ -17,6 +17,7 @@ import com.aserbao.aserbaosandroid.designMode.DesignModeActivity;
 import com.aserbao.aserbaosandroid.functions.FunctionsActivity;
 import com.aserbao.aserbaosandroid.functions.events.onTouch.OnTouchActivity;
 import com.aserbao.aserbaosandroid.functions.ffmpeg.FFmpegActivity;
+import com.aserbao.aserbaosandroid.functions.network.okhttp.OkhttpActivity;
 import com.aserbao.aserbaosandroid.opengl.OpenGlActivity;
 import com.aserbao.aserbaosandroid.other.OthersActivity;
 import com.aserbao.aserbaosandroid.system.SystemActivity;
@@ -62,7 +63,7 @@ public class HomeActivity extends AppCompatActivity {
         mClassBeen.add(new ClassBean("其他", OthersActivity.class));
         mClassBeen.add(new ClassBean("ffmpeg", FFmpegActivity.class));
         mClassBeen.add(new ClassBean("算法", AlgorithmActivity.class));
-        mClassBeen.add(new ClassBean("当前调试的界面", DaggerActivity.class));
+        mClassBeen.add(new ClassBean("当前调试的界面", OkhttpActivity.class));
     }
 
     private void initView() {
--- app/src/main/java/com/aserbao/aserbaosandroid/functions/network/okhttp/OkhttpActivity.java
+++ app/src/main/java/com/aserbao/aserbaosandroid/functions/network/okhttp/OkhttpActivity.java
@@ -1,15 +1,171 @@
 package com.aserbao.aserbaosandroid.functions.network.okhttp;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
 
-import com.aserbao.aserbaosandroid.R;
+import com.aserbao.aserbaosandroid.base.BaseRecyclerViewActivity;
+import com.aserbao.aserbaosandroid.base.beans.BaseRecyclerBean;
 
-public class OkhttpActivity extends AppCompatActivity {
+import java.io.File;
+import java.io.IOException;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Cache;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.Headers;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+
+public class OkhttpActivity extends BaseRecyclerViewActivity {
+    private static final String TAG = "OkhttpActivity";
+    private OkHttpClient mHttpClient;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_okhttp);
+    public void initGetData() {
+        mBaseRecyclerBeen.add(new BaseRecyclerBean("okhttp实现Get请求"));
+        mBaseRecyclerBeen.add(new BaseRecyclerBean("okhttp实现Post请求"));
+        mBaseRecyclerBeen.add(new BaseRecyclerBean("添加请求头的Get请求"));
+        init();
     }
+
+    private void init() {
+        Interceptor interceptor = new Interceptor() {
+            @Override
+            public Response intercept(Chain chain) throws IOException {
+                Request original = chain.request();
+                Request request = original.newBuilder()
+                    .header("User-Agent", "aserbao/(study okhttp)")         // 设置头部代理
+                    .header("Client-Language", Locale.getDefault().getLanguage())
+                    .method(original.method(), original.body())
+                    .build();
+                return chain.proceed(request);
+            }
+        };
+
+        mHttpClient = new OkHttpClient.Builder()
+            .addInterceptor(interceptor)                  //设置拦截器
+            .connectTimeout(10, TimeUnit.SECONDS) //设置建立连接最大可用时间，适用于网络状况正常的情况下，两端连接所用的最长时间。默认值为10s
+            .writeTimeout(20, TimeUnit.SECONDS)   //设置单个IO写入超时时长，默认值10s
+            .readTimeout(20, TimeUnit.SECONDS)    //设置读取超时时间，默认值为10s。
+            .cache(new Cache(new File(Environment.getExternalStorageDirectory(), "cache"), 10 * 1024 * 1024))//设置缓存区地址及大小
+            .build();
+    }
+
+    @Override
+    public void itemClickBack(View view, int position) {
+        switch (position){
+            case 0:
+                simpleOkhttpGet();
+                break;
+            case 1:
+                simpleOkhttpPost();
+                break;
+            case 2:
+                addHeadOkhttpGet();
+                break;
+        }
+    }
+
+//   public static final String sUrl = "https://cn.bing.com/";
+   public static final String sUrl = "http://gank.io/api/today ";
+
+    /**
+     * 简单的Get请求
+     * 步骤：
+     * 1. 构造OkHttpClient客户端
+     * 2. 构造Request
+     * 3. 执行newCall，回调请求结果
+     */
+    public void simpleOkhttpGet(){
+        OkHttpClient okHttpClient = new OkHttpClient.Builder().build();
+        Request request = new Request.Builder().url(sUrl).build();
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Log.e(TAG, "onResponse: " + response.body().string() );
+            }
+        });
+    }
+    /**
+     * 简单的post请求
+     * 步骤：
+     * 1. 添加Post请求参数，构造 FormBody
+     * 2. 构造OkHttpClient客户端
+     * 3. 构造Request
+     * 4. 执行newCall，回调请求结果
+     */
+    public void simpleOkhttpPost(){
+        //1. 添加Post请求参数，构造 FormBody
+        FormBody body = new FormBody.Builder()
+            .add("url", "https://www.jianshu.com/p/53083f782ea2")
+            .add("desc", "一篇好文之Android数据库GreenDao的使用完全解析")
+            .add("who", "aserbao")
+            .add("type", "Android")
+            .add("debug", "false")
+            .build();
+        String postUrl = "https://gank.io/api/add2gank";
+        //2. 构造OkHttpClient客户端
+        OkHttpClient okHttpClient = new OkHttpClient.Builder().build();
+        //3. 构造Request
+        Request request = new Request.Builder().post(body).url(postUrl).build();
+        //4. 执行newCall，回调请求结果
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Log.e(TAG, "onResponse: " + response.body().string() );
+            }
+        });
+    }
+
+    /*
+     * 添加请求头的Get请求
+     */
+    public void addHeadOkhttpGet(){
+        Request request = new Request.Builder()
+            .url(sUrl)
+            .header("User-Agent", "OkHttp Headers.java")         // 设置头部代理
+            .addHeader("Client-Language", Locale.getDefault().getLanguage())
+            .build();
+
+        String header = request.header("User-Agent");
+        Log.e(TAG, "addHeadOkhttpGet: " + header );
+        mHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try (ResponseBody responseBody = response.body()){
+                    if (!response.isSuccessful()) throw new IOException(" sorry， the get request is fail");
+                    Headers responseHeaders = response.headers();
+                    for (int i = 0, size = responseHeaders.size(); i < size; i++) {
+                        Log.e(TAG, "\n onResponse: name = " + responseHeaders.name(i) + " values = " +  responseHeaders.value(i));
+                    }
+                    Log.e(TAG, "\n onResponse: User-Agent = " + response.header("User-Agent") );
+                    Log.e(TAG, "onResponse: " + responseBody.string() );
+                }
+            }
+        });
+    }
+
+
+
 }
