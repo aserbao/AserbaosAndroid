2719e45eeec5b4d0d36ebadafbd496f06ffc9ff0
aserbao <1142803753@qq.com>
2019-07-05 11:21:46 +0800

    提交okhttp

--- app/src/main/java/com/aserbao/aserbaosandroid/functions/network/NetWorkActivity.java
+++ app/src/main/java/com/aserbao/aserbaosandroid/functions/network/NetWorkActivity.java
@@ -2,14 +2,12 @@ package com.aserbao.aserbaosandroid.functions.network;
 
 import com.aserbao.aserbaosandroid.base.BaseActivity;
 import com.aserbao.aserbaosandroid.functions.network.mqtt.MqttActivity;
-import com.aserbao.aserbaosandroid.functions.network.okhttp.OkhttpActivity;
 import com.aserbao.aserbaosandroid.base.beans.ClassBean;
 
 public class NetWorkActivity extends BaseActivity {
 
     @Override
     public void initGetData() {
-        mClassBeen.add(new ClassBean("Htpp网络请求", OkhttpActivity.class));
         mClassBeen.add(new ClassBean("MQTT协议", MqttActivity.class));
     }
 }
