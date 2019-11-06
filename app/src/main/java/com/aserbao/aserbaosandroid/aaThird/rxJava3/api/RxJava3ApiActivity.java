c0e9b59e70c1c4c983d52694dcdf3a02fc924385
aserbao <1142803753@qq.com>
2019-11-06 13:54:46 +0800

    关于RxJava的Api使用。

--- app/src/main/java/com/aserbao/aserbaosandroid/aaThird/rxJava3/api/RxJava3ApiActivity.java
+++ app/src/main/java/com/aserbao/aserbaosandroid/aaThird/rxJava3/api/RxJava3ApiActivity.java
@@ -10,11 +10,20 @@ import com.aserbao.aserbaosandroid.aaThird.rxJava3.download.AndroidScheduler;
 import com.aserbao.aserbaosandroid.comon.base.BaseRecyclerViewActivity;
 import com.aserbao.aserbaosandroid.comon.base.beans.BaseRecyclerBean;
 
+import org.intellij.lang.annotations.Flow;
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import io.reactivex.BackpressureStrategy;
 import io.reactivex.Flowable;
 import io.reactivex.FlowableEmitter;
 import io.reactivex.FlowableOnSubscribe;
 import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Supplier;
 import io.reactivex.schedulers.Schedulers;
 
 public class RxJava3ApiActivity extends BaseRecyclerViewActivity {
@@ -22,6 +31,17 @@ public class RxJava3ApiActivity extends BaseRecyclerViewActivity {
     @Override
     public void initGetData() {
         mBaseRecyclerBeen.add(new BaseRecyclerBean("Flowable 的create创建",0));
+        mBaseRecyclerBeen.add(new BaseRecyclerBean("Flowable 的just创建",1));
+        mBaseRecyclerBeen.add(new BaseRecyclerBean("Flowable 的from创建",2));
+        mBaseRecyclerBeen.add(new BaseRecyclerBean("Flowable 的timer创建",3));
+        mBaseRecyclerBeen.add(new BaseRecyclerBean("Flowable 的interval创建",4));
+        mBaseRecyclerBeen.add(new BaseRecyclerBean("Flowable 的range创建",5));
+        mBaseRecyclerBeen.add(new BaseRecyclerBean("Flowable 的intervalRange创建",6));
+        mBaseRecyclerBeen.add(new BaseRecyclerBean("Flowable 的iterable创建",7));
+
+
+        mBaseRecyclerBeen.add(new BaseRecyclerBean("Flowable 的defer创建",51));
+
         mBaseRecyclerBeen.add(new BaseRecyclerBean("Flowable 的使用",10));
         mBaseRecyclerBeen.add(new BaseRecyclerBean("在线程中使用Flowable ",11));
     }
@@ -32,6 +52,31 @@ public class RxJava3ApiActivity extends BaseRecyclerViewActivity {
             case 0:
                 createFlowable();
                 break;
+            case 1:
+                justFlowable();
+                break;
+            case 2:
+                fromFlowable();
+                break;
+            case 3:
+                timerFlowable();
+                break;
+            case 4:
+                intervalFloawble();
+                break;
+            case 5:
+                rangeFlowable();
+                break;
+            case 6:
+                intervalRangeFlowable();
+                break;
+            case 7:
+                iterableFlowable();
+                break;
+
+            case 51:
+                deferFlowable();
+                break;
             case 10:
                 testFlowable();
                 break;
@@ -40,13 +85,15 @@ public class RxJava3ApiActivity extends BaseRecyclerViewActivity {
                 break;
         }
     }
+
+
     private void createFlowable(){
         Flowable.create(new FlowableOnSubscribe<String>() {
             @Override
             public void subscribe(FlowableEmitter<String> emitter) throws Exception {
                     emitter.onNext("learn rxJava3 from aserbao");
             }
-        },BackpressureStrategy.MISSING)
+        },BackpressureStrategy.ERROR)
             .subscribeOn(Schedulers.newThread())
             .observeOn(AndroidScheduler.mainThread())
             .subscribe(new Consumer<String>() {
@@ -56,6 +103,94 @@ public class RxJava3ApiActivity extends BaseRecyclerViewActivity {
                 }
             });
     }
+
+    private void justFlowable(){
+        Flowable.just(1,2,3,4,5).subscribe(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer integer) throws Throwable {
+                Log.e(TAG, "justFlowable accept: " + integer );
+            }
+        });
+    }
+
+    private void fromFlowable(){
+        String[] inputStrings = {"from1","from2","from3"};
+        Flowable.fromArray(inputStrings)
+            .subscribe(new Consumer<String>() {
+                @Override
+                public void accept(String s) throws Throwable {
+                    Log.e(TAG, "fromFlowable accept: " + s );
+                }
+            });
+    }
+
+    /**
+     * 间隔操作符，固定值返回0
+     */
+    private void timerFlowable(){
+        Flowable.timer(2, TimeUnit.SECONDS)
+            .subscribe(new Consumer<Long>() {
+                @Override
+                public void accept(Long aLong) throws Throwable {
+                    Log.e(TAG, "timerFlowable accept: " + aLong );
+                }
+            });
+    }
+
+    /**
+     * 间隔操作符，从0开始。
+     */
+    private void intervalFloawble(){
+        Flowable<Long> interval = Flowable.interval(2, 1, TimeUnit.MILLISECONDS);
+        interval.subscribe(new Consumer<Long>() {
+                @Override
+                public void accept(Long aLong) throws Throwable {
+                    Log.e(TAG, "intervalFloawble accept: " + aLong );
+                }
+            });
+    }
+
+    private void rangeFlowable(){
+        Flowable.range(1,10).subscribe(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer integer) throws Throwable {
+                Log.e(TAG, "accept: "+integer);
+            }
+        });
+    }
+
+    private void intervalRangeFlowable(){
+        Flowable.intervalRange(1,10,2,1,TimeUnit.SECONDS)
+            .subscribe(new Consumer<Long>() {
+            @Override
+            public void accept(Long aLong) throws Throwable {
+                Log.e(TAG, "intervalRangeFlowable accept: " + aLong );
+            }
+        });
+    }
+
+    private void iterableFlowable(){
+        List<String> arrayList = new ArrayList<>();
+        for (int i = 1; i < 10; i++) {
+            arrayList.add( "第"+ String.valueOf(i) + "个");
+        }
+        Flowable.fromIterable(arrayList).subscribe(new Consumer<String>() {
+            @Override
+            public void accept(String s) throws Throwable {
+                Log.e(TAG, "iterableFlowable accept: " + s );
+            }
+        });
+    }
+
+    private void deferFlowable() {
+        Flowable.defer(new Supplier<Publisher<?>>() {
+            @Override
+            public Publisher<?> get() throws Throwable {
+                return null;
+            }
+        });
+    }
+
 
     private void testFlowable() {
         Flowable.just("Hello world").subscribe(new Consumer<String>() {
--- app/src/main/java/com/aserbao/aserbaosandroid/aaThird/rxJava3/download/RxJavaDownLoadActivity.java
+++ app/src/main/java/com/aserbao/aserbaosandroid/aaThird/rxJava3/download/RxJavaDownLoadActivity.java
@@ -4,6 +4,8 @@ import android.util.Log;
 
 import com.aserbao.aserbaosandroid.comon.base.extend.BaseAboutProgressActivity;
 
+import java.util.concurrent.TimeUnit;
+
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
@@ -11,6 +13,7 @@ import io.reactivex.ObservableSource;
 import io.reactivex.Observer;
 import io.reactivex.Scheduler;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
 import io.reactivex.functions.Supplier;
 import io.reactivex.schedulers.Schedulers;
 
@@ -18,7 +21,7 @@ public class RxJavaDownLoadActivity extends BaseAboutProgressActivity {
     private static final String TAG = "RxJavaDownLoadActivity";
     @Override
     protected void startDownload() {
-        Observable.create(new ObservableOnSubscribe<Integer>() {
+        /*Observable.create(new ObservableOnSubscribe<Integer>() {
             @Override
             public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
                 Log.d(TAG, "subscribe() called with: emitter = [" + emitter + "] ThreadName =" + Thread.currentThread().getName());
@@ -45,7 +48,17 @@ public class RxJavaDownLoadActivity extends BaseAboutProgressActivity {
             @Override
             public void onComplete() {
             }
-        });
+        });*/
+
+        Observable.intervalRange(0,100,15,15, TimeUnit.MILLISECONDS)
+            .subscribeOn(Schedulers.newThread())
+            .observeOn(AndroidScheduler.mainThread())
+            .subscribe(new Consumer<Long>() {
+                @Override
+                public void accept(Long aLong) throws Throwable {
+                    updateProgress(aLong.intValue());
+                }
+            });
     }
 
     @Override
